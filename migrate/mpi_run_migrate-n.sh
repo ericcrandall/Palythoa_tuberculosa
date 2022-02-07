f1f7dfbbf09fb0eeec8eaaa1d66931628af54797
Eric Crandall <eric.d.crandall@gmail.com>
2022-02-06 23:12:32 -0500

    Create a way to write a "modelblock" of molecular evolution models that can be added to the data file. Add it to the datafile. Start the first analysis run.

---
+++ migrate/mpi_run_migrate-n.sh
@@ -0,0 +1,20 @@
+#!
+for r in */
+	do
+		cd $r
+		echo $r
+		date
+		date > date.txt
+			for m in */
+			  do
+				cd $m
+				  echo $m
+				  date
+				  mpirun -np 32 ~/migrate-4.4.4/src/migrate-n-mpi parmfile
+				  sleep 1
+				cd ..
+			  done
+		cd ..
+	done
+
+
--- migrate/run3/mpi_run_migrate-n.sh
+++ migrate/run3/mpi_run_migrate-n.sh
@@ -1,18 +1,11 @@
 #!
-#for s in */
-#do
-#       cd $s
-
-        for m in */
-        do
-                cd $m
-                echo $m
-                screen -S $m
-                exec mpirun -np 32 ~/migrate-4.4.4/src/migrate-n-mpi parmfile
-                sleep 1
-                cd ..
-        done
-#       cd ..
-#done
+for m in */
+  do
+    cd $m
+      echo $m
+      mpirun -np 32 ~/migrate-4.4.4/src/migrate-n-mpi parmfile
+      sleep 1
+    cd ..
+  done
 
 

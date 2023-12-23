1d440e40122a2345bd0e0913725f977377acc575
Eric Crandall <eric.d.crandall@gmail.com>
2023-12-22 22:54:04 -0500

    Update mpi_run_migrate-n.sh

--- migrate/mpi_run_migrate-n.sh
+++ migrate/mpi_run_migrate-n.sh
@@ -10,7 +10,7 @@ for r in rep*/
 				cd $m
 				  echo $m
 				  date
-				  mpirun -np 110 hostfile ~/hosts ~/migrate-5.0.4/src/migrate-n-mpi parmfile
+				  mpirun -np 110 --hostfile ~/hosts ~/migrate-5.0.4/src/migrate-n-mpi parmfile
 				  sleep 1
 				cd ..
 			  done

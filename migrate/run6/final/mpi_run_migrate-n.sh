f3e57276aa898cec28eb9d553007b0bcb94a09ae
ericcrandall <eric.d.crandall@gmail.com>
2024-01-28 22:52:50 -0500

    Download output from run6. Re-arrange some files and add code for model selection

---
+++ migrate/run6/final/mpi_run_migrate-n.sh
@@ -0,0 +1,20 @@
+#!
+for r in  rep5/
+	do
+		cd $r
+		echo $r
+		date
+		date > date.txt
+			for m in */
+			  do
+				cd $m
+				  echo $m
+				  date
+				  mpirun -np 110 --hostfile ~/hosts ~/migrate-5.0.4/src/migrate-n-mpi parmfile
+				  sleep 10
+				cd ..
+			  done
+		cd ..
+	done
+
+
